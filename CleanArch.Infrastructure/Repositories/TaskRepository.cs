8ded77eef85da20b1f08ec7c7389047086e635b5
RamssCR <ramsesgpm@gmail.com>
2025-12-06 21:53:55 -0500

    feat: added repository implementations and dependency injection

--- CleanArch.Infrastructure/Repositories/TaskRepository.cs
+++ CleanArch.Infrastructure/Repositories/TaskRepository.cs
@@ -1,27 +1,39 @@
 ﻿using CleanArch.Domain.Entities;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace CleanArch.Infrastructure.Repositories;
 
-public class TaskRepository : ITaskRepository
+public class TaskRepository(AppDbContext context) : ITaskRepository
 {
-    public Task AddTask(TaskItem task)
-    {
-        throw new NotImplementedException();
-    }
 
-    public Task<IEnumerable<TaskItem>> GetPendingTasks()
-    {
-        throw new NotImplementedException();
-    }
+    /// <summary>
+    /// Adds a tasks to the Task Entity.
+    /// </summary>
+    /// <param name="task">The new task to add.</param>
+    public async Task AddTask(TaskItem task) => await context.Tasks.AddAsync(task);
 
-    public Task SaveChangesAsync()
-    {
-        throw new NotImplementedException();
-    }
+    /// <summary>
+    /// Gets all pending tasks.
+    /// </summary>
+    /// <returns>The found pending tasks.</returns>
+    public async Task<IReadOnlyList<TaskItem>> GetPendingTasks() =>
+        await context.Tasks
+            .Where(task => task.IsCompleted == false)
+            .ToListAsync();
 
-    public Task<TaskItem?> GetTaskById(Guid id)
-    {
-        throw new NotImplementedException();
-    }
+    /// <summary>
+    /// Returns a task by its ID or null if not found.
+    /// </summary>
+    /// <param name="id">The ID to search with.</param>
+    /// <returns>The found task or null if not found.</returns>
+    public async Task<TaskItem?> GetTaskById(Guid id) =>
+        await context.Tasks.FindAsync(id);
+    
+    /// <summary>
+    /// Saves changes made to an entity.
+    /// </summary>
+    public async Task SaveChangesAsync() => await context.SaveChangesAsync();
+
 }
