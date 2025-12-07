3cef6839fbab264720ec1af97e37e9e5d447a963
RamssCR <ramsesgpm@gmail.com>
2025-12-06 21:53:17 -0500

    fix: replaced IEnumerable for IReadOnlyList

--- CleanArch.Domain/Interfaces/ITaskRepository.cs
+++ CleanArch.Domain/Interfaces/ITaskRepository.cs
@@ -4,7 +4,7 @@ using Entities;
 public interface ITaskRepository
 {
     Task AddTask(TaskItem task);
-    Task<IEnumerable<TaskItem>> GetPendingTasks();
+    Task<IReadOnlyList<TaskItem>> GetPendingTasks();
     Task<TaskItem?> GetTaskById(Guid id);
     Task SaveChangesAsync();
 }
