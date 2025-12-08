6c56e3ebe0b3173dfc207e5db6310159e78db4cb
RamssCR <ramsesgpm@gmail.com>
2025-12-08 17:18:30 -0500

    feat: created DI handler for the application layer

---
+++ CleanArch.Application/DependencyInjection/ApplicationServiceRegistration.cs
@@ -0,0 +1,22 @@
+﻿namespace CleanArch.Application.DependencyInjection;
+using UseCases.Task.CreateTask;
+using UseCases.Task.GetTaskById;
+using UseCases.Task.GetPendingTasks;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class ApplicationServiceRegistration
+{
+    /// <summary>
+    /// Extends the service builder method to add
+    /// transient handlers from the application layer.
+    /// </summary>
+    /// <param name="services">The service builder.</param>
+    /// <returns>The service with the new extended method.</returns>
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddTransient<GetPendingTasksHandler>();
+        services.AddTransient<GetTaskByIdHandler>();
+        services.AddTransient<CreateTaskHandler>();
+        return services;
+    }
+}
