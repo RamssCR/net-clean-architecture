2f60a0ebdfae4908db92f21180b147e4c6096c4c
RamssCR <ramsesgpm@gmail.com>
2025-12-08 17:21:31 -0500

    feat: created application use for getting all pending tasks

---
+++ CleanArch.Application/UseCases/Task/GetPendingTasks/GetPendingTasksHandler.cs
@@ -0,0 +1,24 @@
+﻿using CleanArch.Application.Dtos.Task.GetPendingTasks;
+using CleanArch.Domain.Interfaces;
+
+namespace CleanArch.Application.UseCases.Task.GetPendingTasks;
+
+public class GetPendingTasksHandler(ITaskRepository repository)
+{
+    /// <summary>
+    /// Returns a list of pending tasks.
+    /// </summary>
+    /// <returns>The list of found pending tasks.</returns>
+    public async Task<IReadOnlyList<GetPendingTaskResponse>> HandleAsync()
+    {
+        var tasks = await repository.GetPendingTasks();
+        return tasks
+            .Select(task => new GetPendingTaskResponse(
+                task.Id,
+                task.Title.ToString(),
+                task.Description,
+                task.IsCompleted
+            ))
+            .ToList();
+    }
+}
