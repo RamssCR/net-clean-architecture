f374b429738d9d02734e5be83deaf3e9ef97d07c
RamssCR <ramsesgpm@gmail.com>
2025-12-08 22:32:28 -0500

    fix: defined value object as owned entities

--- CleanArch.Infrastructure/Persistence/AppDbContext.cs
+++ CleanArch.Infrastructure/Persistence/AppDbContext.cs
@@ -5,4 +5,23 @@ using Domain.Entities;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     public DbSet<TaskItem> Tasks => Set<TaskItem>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        DefineValueObjects(modelBuilder);
+    }
+
+    private static void DefineValueObjects(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<TaskItem>(entity =>
+        {
+            entity.OwnsOne(task => task.Title, title =>
+            {
+                title.Property(task => task.Title)
+                    .HasColumnName("Title")
+                    .IsRequired();
+            });
+        });
+    }
 }
